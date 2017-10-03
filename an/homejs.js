1609b4c0b13ce707fd4900fa47589671f065be91
antimajain <antima.jain96@gmail.com>
2017-10-03 16:57:30 +0530

    Add files via upload

--- an/homejs.js
+++ an/homejs.js
@@ -1,1 +1,28 @@
-sd
+//alert("hello world");
+
+// debugger;
+ function load(){
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function(){
+          if (this.readyState == 4 && this.status == 200) {
+              var data = JSON.parse(this.response);
+              document.getElementById("img1").src = data[0].img;
+              document.getElementById("img2").src = data[1].img;
+              document.getElementById("img3").src = data[2].img;
+              document.getElementById("img4").src = data[3].img;
+              document.getElementById("img5").src = data[4].img;
+              document.getElementById("img6").src = data[5].img;
+
+
+
+
+          }
+      };
+      xmlhttp.open("GET", "https://raw.githubusercontent.com/antimajain/webtemp2/master/an/json1.json", true);
+      xmlhttp.send();
+
+//     console.log("Hello");
+ }
+
+
+
